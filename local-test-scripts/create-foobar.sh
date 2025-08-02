6b3516ad030f0d5a7d179ad8927ea61af746c997
Leo Domeneghetti <leodomeneghetti@gmail.com>
2025-08-02 02:34:43 -0300

    Testing kubectl action

--- local-test-scripts/create-foobar.sh
+++ local-test-scripts/create-foobar.sh
@@ -5,7 +5,7 @@ sudo kubectl apply -f foo-app/foo-deployment.yaml  -n goodnotes-takehome
 sudo kubectl apply -f foo-app/foo-service.yaml  -n goodnotes-takehome
 sudo kubectl apply -f foo-app/foo-ingress.yaml  -n goodnotes-takehome
 # Create Bar
-sudo kubectl apply -f bar-app/bar-deployment.yaml  -n goodnotes-takehome
+sudo kubectl apply -f k8s-resources/bar-app/bar-deployment.yaml  -n goodnotes-takehome
 sudo kubectl apply -f bar-app/bar-service.yaml  -n goodnotes-takehome
 sudo kubectl apply -f bar-app/bar-ingress.yaml  -n goodnotes-takehome
 
@@ -13,3 +13,4 @@ cd ../
 
 # curl --resolve foo.localhost:8080:172.18.0.3 http://foo.localhost:8080
 # curl --resolve bar.localhost:8080:172.18.0.3 http://bar.localhost:8080
+run: 
